ebb55f10075afc7a9c994fe2efc88eb9c788739b
JeyJeyG <jeyjeygraham@gmail.com>
2023-09-20 09:49:31 +0000

    Topbar done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,48 +1,41 @@
-import { Search, Person, Chat, Notifications } from "@mui/icons-material";
 import "./Topbar.css";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">i-social</span>
+        <span className="logo">JeyJey</span>
       </div>
       <div className="topbarCenter">
-        <div className="searchBar">
-          <Search />
-          <input placeholder="Search for friends" className="searchInput" />
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
         </div>
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-          <span className="topbarLinks">Homepage</span>
-          <span className="topbarLinks">Time-line</span>
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
         </div>
         <div className="topbarIcons">
           <div className="topbarIconItem">
             <Person />
-            <span className="topbarIconBadge">
-              1
-            </span>
+            <span className="topbarIconBadge">1</span>
           </div>
-        </div>
-        <div className="topbarIcons">
           <div className="topbarIconItem">
             <Chat />
-            <span className="topbarIconBadge">
-              1
-            </span>
+            <span className="topbarIconBadge">2</span>
           </div>
-        </div>
-        <div className="topbarIcons">
           <div className="topbarIconItem">
             <Notifications />
-            <span className="topbarIconBadge">
-              2 
-            </span>
+            <span className="topbarIconBadge">1</span>
           </div>
         </div>
-        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
       </div>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,6 @@ import Topbar from '../../components/topbar/Topbar'
 
 export default function Home() {
   return (
-    <Topbar/>
+    <Topbar />
   )
 }
